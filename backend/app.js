a88e95a8194502da6fc19e65db0e7fbf21239933
vyducdatls04 <vyducdatls04@gmail.com>
2026-05-14 00:33:17 +0700

    update axios config and app.js

--- backend/app.js
+++ backend/app.js
@@ -23,7 +23,7 @@ const app = express();
 ========================= */
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://smart-study-planner-do7p.onrender.com"
+  "https://smart-study-planner-4ipg.vercel.app"
   
 ];
 
